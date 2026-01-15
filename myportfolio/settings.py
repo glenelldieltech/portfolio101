565ec848ff6ae401f0cf2c358b112854dcc5e1e8
natsuno962 <126125877+natsuno962@users.noreply.github.com>
2026-01-15 15:56:49 +0800

    Update settings.py

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -130,7 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # =========================
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 EMAIL_USE_SSL = True
 EMAIL_USE_TLS = False
 EMAIL_HOST_USER = 'glenelldieltech@gmail.com'
