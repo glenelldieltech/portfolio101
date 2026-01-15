e67644b888ea5e9a1347ff9743630cd474415800
natsuno962 <126125877+natsuno962@users.noreply.github.com>
2026-01-15 13:06:29 +0800

    Update settings.py

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -130,10 +130,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # =========================
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'glenelldieltech@gmail.com'      # iyong Gmail
-EMAIL_HOST_PASSWORD = 'yryouzhucpdqgpkz'     # Gmail App Password
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
+EMAIL_HOST_USER = 'glenelldieltech@gmail.com'
+EMAIL_HOST_PASSWORD = 'yryouzhucpdqgpkz'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+# =========================
+# Static files settings (HUWAG BURAHIN)
+# =========================
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
